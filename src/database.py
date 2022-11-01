30a6defc25093d407d1d6ab89c83240ec81a0172
JeanLuca999 <jeanescolaetec2019@gmail.com>
2022-11-01 11:53:35 -0300

    setup initial database

---
+++ src/database.py
@@ -0,0 +1,14 @@
+from sqlite3 import connect
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./app.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
