059d5adcadf698140badef6b10b0c3808b04ae98
Breno Zukowski <brenozukotech@gmail.com>
2022-11-06 15:18:10 -0300

    feat(posts): add relationship between users and posts

--- src/models.py
+++ src/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, UniqueConstraint
+from sqlalchemy import Column, Integer, String, UniqueConstraint, ForeignKey
+from sqlalchemy.orm import relationship
 from .database import Base
 
 
@@ -9,10 +10,12 @@ class Users(Base):
     name = Column(String)
     email = Column(String)
     password = Column(String)
+    children = relationship("Posts")
 
 
 class Posts(Base):
     __tablename__ = "posts"
 
     id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
     body = Column(String)
